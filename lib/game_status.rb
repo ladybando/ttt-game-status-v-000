be6ef22aabef6db801560aee3a2ce3cef11a3bf7
Ayana Bando <ayanaflax@yahoo.com>
2017-11-02 16:14:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,13 +9,13 @@ WIN_COMBINATIONS= [ [0,1,2], [3,4,5], [6,7,8],
 [0,3,6], [1,4,7], [2,5,8]]
 
 def won?(board)
-    
+
     if board[index] == " " || board[index] == "" || board[index] == nil
       false
     else
     end
 
-      
+
 
   end
 
