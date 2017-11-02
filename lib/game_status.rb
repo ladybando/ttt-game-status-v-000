cfd6b50f91ee12c0def8af8d69a026a0204f867c
Ayana Bando <ayanaflax@yahoo.com>
2017-11-02 15:55:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,11 +10,11 @@ WIN_COMBINATIONS= [ [0,1,2], [3,4,5], [6,7,8],
 
 def won?(board)
   def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
-end
-end
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      false
+    else
+    end
+  end
 
 end
 
